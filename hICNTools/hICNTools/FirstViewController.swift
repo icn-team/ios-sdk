b0c3197a38a1c478a4967e683e87442b31adbb78
Angelo Mantellini <angelo.mantellini@cisco.com>
2020-03-12 13:01:49 +0100

    first commit hICNTools

---
+++ hICNTools/hICNTools/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  hICNTools
+//
+//  Created by manangel on 3/12/20.
+//  Copyright © 2020 manangel. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
